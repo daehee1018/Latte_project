b385295b693ff7471d8dfe4730fc19eb4475b390
daehee1018 <handh011018@gmail.com>
2023-11-08 17:34:49 +0900

    헤드파일 생성

---
+++ head.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <conio.h>
+#include <time.h>
+#include <string.h>
+
+
+int snack;
+int rock(void);
+void blackjack(void);
+void title(void);
+void shop(void);
