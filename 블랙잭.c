b66cc66f6a0b532a83fd988c66e95e14e5ff2f32
daehee1018 <handh011018@gmail.com>
2023-11-15 16:43:25 +0900

    블랙잭 오류수정 2

--- 블랙잭.c
+++ 블랙잭.c
@@ -6,7 +6,7 @@ void blackjack() {
     printf("블랙잭 게임\n");
     printf("베팅할 간식 수를 입력하세요 \n");
     printf("입력 :  ");
-    scanf_s(" %d\n", &snack);
+    scanf(" %d\n", &snack);
     printf("입력한 개수 : %d개\n", snack);
     printf("게임이 시작됩니다. \n");
     printf("┌────────┐\n");
