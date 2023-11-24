3e47ac76489169442366921c2b3629bd2bc6ac99
daehee1018 <handh011018@gmail.com>
2023-11-25 01:54:27 +0900

    헤더파일에 choice함수 추가

--- head.h
+++ head.h
@@ -5,7 +5,6 @@
 #include <time.h>
 #include <string.h>
 
-
 int snack;
 int rock(void);
 void choice_cat(void);
@@ -17,3 +16,4 @@ void start_blackjack(void);
 char ID[20];//아이디
 char id(void);// 아이디 등록
 void gotoxy(int x, int y);
+void choice(void);
