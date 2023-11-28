4f45ca93b19e93b71f361414c7690b985733a0bb
ayuuuneee <mayunrk1842@gmail.com>
2023-11-28 13:26:56 +0900

    상점 메뉴 화면, 모자 안경 함수 생성

--- 상점.c
+++ 상점.c
@@ -16,12 +16,39 @@ void shop()
 	printf("                                             ▲  고양이를 선택하세요 ▲ ");
 	scanf("%d", &selMenu);
 
-	if (selMenu == 1) { system("cls"); cat1(); }
-	if (selMenu == 2) { system("cls"); cat2(); }
-	if (selMenu == 3) { system("cls"); cat3(); }
+	if (selMenu == 1) { system("cls"); hat_shop(); }
+	if (selMenu == 2) { system("cls"); glasses_shop(); }
+	if (selMenu == 3) { system("cls"); title(); }
 
 
 
 }
 
+void hat_shop()
+{
+	int selHat;
+	printf("                                       ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓\n");
+	printf("                                       ┃           모자 구매하기           ┃\n");
+	printf("                                       ┃                                   ┃\n");
+	printf("                                       ┃       1. 마법사 모자 구매하기     ┃\n");
+	printf("                                       ┃         2. 왕관 구매하기          ┃\n");
+	printf("                                       ┃        3. 상점으로 돌아가기       ┃\n");
+	printf("                                       ┗━━━━━━━━━━-━━━━━━━━━━━━━━━-━━━━━━━━┛\n");
+	printf("                                            ▲  원하는 항목을 선택하세요 ▲ ");
+	scanf("%d", &selHat);
+	if (selHat == 1) { system("cls"); magic_hat(); }
+	if (selHat == 2) { system("cls"); crown(); }
+	if (selHat == 3) { system("cls"); shop(); }
+}
 
+glasses_shop(){
+
+}
+
+magic_hat() {
+
+}
+
+crown() {
+
+}
