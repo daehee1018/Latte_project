e6752377801ee425a52ee0b0085bc81034aaa17f
daehee1018 <handh011018@gmail.com>
2023-12-04 19:16:57 +0900

    메뉴 인터페이스 수정

--- 메인화면.c
+++ 메인화면.c
@@ -1,21 +1,24 @@
 #include "head.h"
 void title() {
 
-    system("mode con cols=100 lines=40 ");
+    system("mode con cols=150 lines=55 ");
     int start_num;
-    printf("----------Cat Snack Collection Game----------\n");
-    printf("|1. 가위바위보 게임                         |\n");
-    printf("|2. 블랙잭 게임                             |\n");
-    printf("|3. 상점                                    |\n");
-    printf("|4. 초기화                                  |\n");
-    printf("---------------------------------------------\n");
+   
+    printf("                                       ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓\n");
+    printf("                                       ┃              메뉴 선택            ┃\n");
+    printf("                                       ┃                                   ┃\n");
+    printf("                                       ┃         1. 가위바위보 게임        ┃\n");
+    printf("                                       ┃         2. 블랙잭 게임            ┃\n");
+    printf("                                       ┃         3. 상점                   ┃\n");
+    printf("                                       ┃         4. 초기화                 ┃\n");
+    printf("                                       ┗━━━━━━━━━━-━━━━━━━━━━━━━━━-━━━━━━━━┛\n");
     printf("\n");
     printf("\n");
     printf("\n");
     printf("\n");
     printf("\n");
-    printf("---------------------------------------------\n");
-    printf("\t 이동할 화면의 숫자를 입력하세요!\n");
+    printf("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━-━━━━━━━━━━━━━━━-━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n");
+    printf("                                           ▲  원하는 메뉴를 선택하세요 >  ");
     scanf_s("%d", &start_num);
     if (start_num == 1) { system("cls"); rock(); }
     else if (start_num == 2) { system("cls"); start_blackjack(); }
